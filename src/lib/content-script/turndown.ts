1cc36704a9935b87f09e80a79d21048da9cfc3c3
Aron Homberg <info@aron-homberg.de>
2024-06-16 21:08:06 +0200

    fix: post-processing for markdown parsing issues

--- src/lib/content-script/turndown.ts
+++ src/lib/content-script/turndown.ts
@@ -47,6 +47,114 @@ turndownService.addRule("scripting", {
   replacement: (content: string) => "",
 });
 
-export const turndown = (html: string): string =>
+export const fixLinks = (markdown: string): string =>
+  markdown
+    .replace(
+      /\[\s*(.+?)\s*\]\((.+?)\)/gs,
+      (match, p1, p2) =>
+        `[${markdownTrim(p1.replace(/\s*\n\s*/g, " "))}](${p2})`,
+    )
+    .replace(/(\S)\[(.+?)\]\((.+?)\)(\S)/g, "$1 [$2]($3) $4") // Ensure space around links
+    .replace(/(\S)\[(.+?)\]\((.+?)\)/g, "$1 [$2]($3)") // Space after the link
+    .replace(/\[(.+?)\]\((.+?)\)(\S)/g, "[$1]($2) $3") // Space before the link
+    .replace(
+      /\[\s*\]\((.+?)\s+"(.+?)"\)/g, // Remove leading and trailing whitespace inside title, link text (pass 1)
+      (match, p1, p2) =>
+        `[${markdownTrim(p2)}](${markdownTrim(p1)} "${markdownTrim(p2)}")`,
+    )
+    .replace(
+      /\[(.+?)\]\((.+?)\s+"(.+?)"\)/g, // Remove leading and trailing whitespace inside title, link text (pass 2)
+      (match, p1, p2, p3) =>
+        `[${markdownTrim(p1)}](${markdownTrim(p2)} "${markdownTrim(p3)}")`,
+    );
+
+export const fixHeadingsWithoutNewlines = (markdown: string): string =>
+  markdown.replace(/(^|\n)([^\n]*?)(?=\n?#)/g, (match, p1, p2) => {
+    // Trim the leading and trailing whitespace
+    const trimmed = markdownTrim(p2);
+    return trimmed ? `${p1}${trimmed}\n\n` : `${p1}${trimmed}`;
+  });
+
+// trims leading and trailing whitespace, newline, and other non-printable characters without affecting UTF-8 symbols.
+export const markdownTrim = (markdown: string): string =>
+  markdown.replace(/^[\s\p{C}]+|[\s\p{C}]+$/gu, "");
+
+export const postProcessMarkdown = (markdown: string): string =>
+  removeNonStandardProtocolHrefs(
+    fixHeadingsWithoutNewlines(
+      trimLeadingElementWhitespace(
+        fixMissingLinkWhenTitleIsSet(fixHeadingLinks(fixLinks(markdown))),
+      ),
+    ),
+  );
+
+export const removeNonStandardProtocolHrefs = (markdown: string): string =>
+  markdown.replace(
+    /(^|\n)([^\n]*?)(?=\n?#)|\[[^\]]*\]\((?!(http:\/\/|https:\/\/))[^\)]*\)/g,
+    (match, p1, p2) => {
+      if (p2 !== undefined) {
+        // This handles the heading formatting
+        const trimmed = markdownTrim(p2);
+        return `${p1}${trimmed}\n\n`;
+      }
+      // This handles the non-http/https link removal
+      return "";
+    },
+  );
+
+export const fixHeadingLinks = (markdown: string): string =>
+  // Move any # starting directly after [ to the position before the last \n\n
+  markdown.replace(/(\[#+\s*)(.+?)\s*\](\(.+?\))/g, (match, p1, p2, p3) => {
+    const headingText = markdownTrim(p2);
+    const headingLevel = markdownTrim(p1).replace("[", "");
+    return `${headingLevel} ${headingText}\n\n[${headingText}]${p3}`;
+  });
+
+export const fixMissingLinkWhenTitleIsSet = (markdown: string): string =>
+  markdown.replace(
+    /\[\s*\]\((.+?)\s+"(.+?)"\)/g,
+    (match, p1, p2) => `[${p2}](${p1} "${p2}")`,
+  );
+
+export const trimLeadingElementWhitespace = (markdown: string): string =>
+  markdown.replace(/^\s+([#\[])/gm, (match) => markdownTrim(match));
+
+const escapeMarkdown = (text: string): string => {
+  return text.replace(/([\\`*{}[\]()#+\-.!_>])/g, "$1");
+};
+
+// function to transform html links to markdown links
+export const htmlLinkToMarkdown = (html: string): string => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, "text/html");
+  const anchor = doc.querySelectorAll("a");
+
+  if (!anchor) {
+    return html;
+  }
+
+  for (let i = 0; i < anchor.length; i++) {
+    const href = decodeURIComponent(anchor[i].getAttribute("href") || "");
+    const title = anchor[i].getAttribute("title") || "";
+    const text = anchor[i].textContent || "";
+
+    const escapedTitle = escapeMarkdown(title);
+    const escapedText = escapeMarkdown(text);
+
+    html = html.replace(
+      anchor[i].outerHTML,
+      `[${markdownTrim(escapedText)}](${markdownTrim(href)} "${markdownTrim(
+        escapedTitle,
+      )}")`,
+    );
+  }
+  return html;
+};
+
+export const turndown = (html: string): string => {
+  //const preprocessed = htmlLinkToMarkdown(html);
   // remove buggy characters; TODO: fix with data format change
-  turndownService.turndown(html.replace(/[®©™]/g, ""));
+  return postProcessMarkdown(
+    turndownService.turndown(html.replace(/[®©™]/g, "")),
+  );
+};
